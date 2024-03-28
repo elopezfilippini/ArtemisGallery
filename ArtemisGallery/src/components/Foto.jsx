e073c57af6b90972359cc3a17f7b902061b0264d
elopezfilippini <elopezfilippini@gmail.com>
2024-03-28 11:40:16 -0300

    2

--- ArtemisGallery/src/components/Foto.jsx
+++ ArtemisGallery/src/components/Foto.jsx
@@ -9,17 +9,18 @@ const Foto = () => {
   const monstruo = fotos.find(foto => foto.id === Number(id));
 
   return (
-    <div className='mt-10'>
+    <div className='mt-5 mb-5'>
       <div className="flex justify-center items-center">
         <LiaChevronCircleLeftSolid
           onClick={() => navigate("/galeria")}
-          className="text-blue-900 md:text-[100px] text-[23px]cursor-pointer hover:text-purple-500"
+          className="text-blue-900 md:text-[70px] text-[53px]cursor-pointer hover:text-purple-500"
         />
       </div>
       <h1 className='font-caveat text-4xl md:text-7xl text-center mt-4'>" {monstruo.titulo} "</h1>
       <div className="flex justify-center mt-4">
         <img className="object-contain max-w-[375px]  md:max-w-full" src={`../src/assets/galeriaTemi/s (${id}).jpeg`} alt={`Foto ${id}`} />
       </div>
+      <span></span>
     </div>
   );
 }
