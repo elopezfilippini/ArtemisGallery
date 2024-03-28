05dc52239efcf51f2a0b0c2a43aa781670667e40
elopezfilippini <elopezfilippini@gmail.com>
2024-03-28 11:22:20 -0300

    Boton de volver

--- ArtemisGallery/src/components/Foto.jsx
+++ ArtemisGallery/src/components/Foto.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { useParams } from "react-router-dom";
-import fotos from "../Utils/rutasgaleria.json"
+import { useParams, useNavigate } from "react-router-dom";
+import fotos from "../Utils/rutasgaleria.json";
+import { LiaChevronCircleLeftSolid } from "react-icons/lia";
 
 const Foto = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const monstruo = fotos.find(foto => foto.id === Number(id));
 
   return (
-    <div className='mt-10 justify-center  items-center md:h-full md:y-[1000px]'>
-      <h1 className='font-caveat text-4xl md:text-9xl'>"{monstruo.titulo}"</h1>
-      <div className="mt-4 flex justify-center items-center">
+    <div className='mt-10'>
+      <div className="flex justify-center items-center">
+        <LiaChevronCircleLeftSolid
+          onClick={() => navigate("/galeria")}
+          className="text-blue-900 text-[100px] cursor-pointer hover:text-purple-500"
+        />
+      </div>
+      <h1 className='font-caveat text-4xl md:text-7xl text-center mt-4'>" {monstruo.titulo} "</h1>
+      <div className="flex justify-center mt-4">
         <img className="object-contain max-w-full max-h-full" src={`../src/assets/galeriaTemi/s (${id}).jpeg`} alt={`Foto ${id}`} />
       </div>
     </div>
